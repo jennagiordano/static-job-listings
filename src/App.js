9e5c5c361138c838233d8d146602d514902fbe42
jennagiordano <31827859+jennagiordano@users.noreply.github.com>
2020-10-07 13:55:50 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  Nav,
+  Navbar,
+  NavDropdown,
+  Button,
+  Form,
+  FormControl,
+} from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" expand="lg" id="navbar">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      </Navbar>
     </div>
   );
 }
